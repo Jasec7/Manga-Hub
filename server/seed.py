fd36f9b30b170b216b3d2f82427f2a064342b991
Jasec <jasec_7@hotmail.com>
2025-12-24 22:43:08 -0800

    add seed data

--- server/seed.py
+++ server/seed.py
@@ -22,6 +22,7 @@ if __name__ == '__main__':
         Review.query.delete()
         Manga.query.delete()
         Chapter.query.delete()
+        Review.query.delete()
 
         print("creating mangas...")
         m1 = Manga(title = 'Jujutsu Kaisen', creator = "Gege Akutami", release_year = 2018)
@@ -44,9 +45,21 @@ if __name__ == '__main__':
         mc4 = MangaChapter(chapter_number = 1, manga = m4, chapter = c4)
         manga_chapters = [mc1, mc2, mc3, mc4]
 
+        print("Creating reviews")
+        rw1 = Review(reviewer = 'Sean_2275', comment = "Amazing fight scenes, super entertaining", rating = 5, manga = m1)
+        rw2 = Review(reviewer = 'Antoine', comment = "Great anime, but it needs more chapters", rating = 4, manga = m1)
+        rw3 = Review(reviewer = 'Freccks', comment = "One of the greates mangas", rating = 5, manga = m2)
+        rw4 = Review(reviewer = 'Kat22', comment = "Overrated", rating = 3, manga = m2)
+        rw5 = Review(reviewer = 'Pat', comment = "I can't have enough of it, I love it", rating = 5, manga = m3)
+        rw6 = Review(reviewer = 'Anita-kun', comment = "Good manga but bad power scale development", rating = 4, manga = m3)
+        rw7 = Review(reviewer = 'Saitama_90', comment = "Hilarious love the story", rating = 5, manga = m4)
+        rw8 = Review(reviewer = 'Xenos', comment = "I can't wait for the next chapters! I'm obsses", rating = 5, manga = m4)
+        reviews = [rw1, rw2, rw3, rw4, rw5, rw6, rw7, rw8]
+
         db.session.add_all(mangas)
         db.session.add_all(chapters)
         db.session.add_all(manga_chapters)
+        db.session.add_all(reviews)
         db.session.commit()
 
         print("Seeding done!")

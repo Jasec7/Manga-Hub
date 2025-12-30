cebac380aedaf4c25e0e32dcb57ddf4f9f213fd8
Jasec <jasec_7@hotmail.com>
2025-12-30 03:17:25 -0800

    modify MangaForm

--- client/src/components/MangaForm.js
+++ client/src/components/MangaForm.js
@@ -10,13 +10,15 @@ function MangaForm({onAddMangas}){
 
     function handleSubmit(e){
         e.preventDefault();
+        const year = parseInt(formData.release_year);
 
         fetch("/mangas",{
             method:"POST",
             headers:{
                 "Content-Type":"application/json"
             },
-            body:JSON.stringify(formData)
+            body:JSON.stringify({title:formData.title, creator:formData.creator, release_year:parseInt(year)
+            })
         })
         .then((r) => r.json())
         .then((newManga) => {onAddMangas(newManga);
@@ -25,7 +27,7 @@ function MangaForm({onAddMangas}){
     }
     return(
         <div className='new-manga-form'>
-        <h2>New Manga</h2>
+        <h3>New Manga</h3>
         <form onSubmit={handleSubmit}>
             <input type='text' name='title' placeholder='title'
             value={formData.title}
@@ -35,7 +37,7 @@ function MangaForm({onAddMangas}){
             onChange={(e) => setFormData({...formData, creator: e.target.value})}/>
              <input type='text' name='release_year' placeholder='release_year'
             value={formData.release_year}
-            onChange={(e) => setFormData({...formData, release_year:parseInt(e.target.value)})}/>
+            onChange={(e) => setFormData({...formData, release_year:e.target.value})}/>
             <button type='submit'>Add Manga</button>
         </form>
         </div>

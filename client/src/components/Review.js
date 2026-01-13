59a2a0e5fa9f91090158dc917aa9c3691de30d53
Jasec <jasec_7@hotmail.com>
2026-01-13 03:39:53 -0800

    add toggle feature to Review.js

--- client/src/components/Review.js
+++ client/src/components/Review.js
@@ -1,13 +1,22 @@
-
+import React, {useState} from "react";
 
 function Review({id, reviewer, comment, rating, onDelete, onReview}){
+    const [isToggle, setIstoggle] = useState(false);
+
+    function handleToggle(){
+        setIstoggle(!isToggle)
+    };
+    console.log('pushed:',isToggle)
     return(
         <div>
-            <h4>{reviewer}</h4>
+            <button onClick={handleToggle}>
+                {isToggle ? 'Hide review' : 'Show review'}
+            </button>
+            {isToggle && ( <div><h4>{reviewer}</h4>
             <p>{comment}</p>
             <p>Rating: {rating}</p>
-            <button onClick={() => onReview(id, reviewer, comment, rating)}>Edit</button>
-            <button onClick={() => onDelete(id)}>Delete</button>
+            <button onClick={() => onReview(id, {reviewer, comment, rating})}>Edit</button>
+            <button onClick={() => onDelete(id)}>Delete</button> </div>)}
         </div>
     )
 }

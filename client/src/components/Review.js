0d6f5f04d267145a178e4fad4e4a1437c693ab26
Jasec <jasec_7@hotmail.com>
2026-01-14 14:41:57 -0800

    modify Review.js

--- client/src/components/Review.js
+++ client/src/components/Review.js
@@ -1,16 +1,26 @@
-
+import React, {useState} from "react";
 
 function Review({id, reviewer, comment, rating, onDelete, onReview}){
-    
+    const [isEdit, setIsEdit] = useState(false)
+     
     return(
         <div>
+             {!isEdit ? (
+            <> 
             <h4>{reviewer}</h4>
             <p>{comment}</p>
             <p>Rating: {rating}</p>
+            <button onClick={() => onDelete(id)}>Delete</button>
             <button onClick={() => onReview(id, {reviewer, comment, rating})}>Edit</button>
-            <button onClick={() => onDelete(id)}>Delete</button> 
+            </>
+            ) : (
+                <>
+                <p>Edit Mode</p>
+                <button onClick={() => setIsEdit(false)}>Cancel</button>
+                </>
+            )}
         </div>
-    )
+    );
 }
 
 export default Review;

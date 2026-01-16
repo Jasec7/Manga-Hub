1f06f851cce078a72cf4d86821194ee858923b66
Jasec <jasec_7@hotmail.com>
2026-01-16 04:03:37 -0800

    modify index.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 import MangaPage from "./MangaPage";
+import {Outlet} from 'react-router-dom';
+
 function App() {
   return(
     <div>
     <MangaPage/>
+    <Outlet/>
     </div>
   )
 }
---
+++ client/src/components/Home.js
@@ -0,0 +1,10 @@
+
+
+function Home(){
+    return(
+        <div>
+            <h1>Welmcome to Manga Hub</h1>
+        </div>
+    )
+}
+export default Home;
